e7b18b2e0574c0ce824bef4b52afc6369c772ac0
Asha Geyon <103749286+Natpol50@users.noreply.github.com>
2024-10-22 11:18:02 +0200

    LED2

--- LED/TestALED/OnLED.cpp
+++ LED/TestALED/OnLED.cpp
@@ -1,3 +1,4 @@
+
 /* 
  * Example of using the ChainableRGB library for controlling a Grove RGB.
  * This code cycles through all the colors in an uniform way. This is accomplished using a HSB color space. 
@@ -12,9 +13,7 @@
 
 extern "C" void __attribute__((weak)) yield(void) {}
 
-#define NUM_LEDS  5
-
-ChainableLED leds(7, 8, NUM_LEDS);//defines the pin used on arduino.
+ChainableLED leds(0, 7, 8);//defines the pin used on arduino.
 
 void setup()
 {
@@ -26,18 +25,13 @@ boolean up = true;
 
 void loop()
 {
-  for (byte i=0; i<NUM_LEDS; i++)
-    leds.setColorHSB(i, hue, 1.0, 0.5);
+
+  leds.setColorRGB(0, 255, 50, 0);
     
-  delay(50);
+  delay(100);
     
-  if (up)
-    hue+= 0.025;
-  else
-    hue-= 0.025;
-    
-  if (hue>=1.0 && up)
-    up = false;
-  else if (hue<=0.0 && !up)
-    up = true;
+  leds.setColorRGB(0, 0, 0, 0);
+
+  delay(100);
 }
+
